22aeea4ca1c1224c5f49d9b335378d68ca7931fc
quyenlt <lethoquyen2002@gmail.com>
2025-12-28 19:05:54 +0700

    Add new API endpoints for chirp validation, readiness, metrics, and reset. Refactor server to improve modularity and include bad word filtering in chirps.

--- main.go
+++ main.go
@@ -1,81 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
 )
 
 type apiConfig struct {
-	fileServerHit atomic.Int32
-}
-
-const metricsTemplate = `
-<html>
-  <body>
-    <h1>Welcome, Chirpy Admin</h1>
-    <p>Chirpy has been visited %d times!</p>
-  </body>
-</html>
-`
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileServerHit.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	hits := cfg.fileServerHit.Load()
-
-	w.Header().Set("Content-Type", "text/html")
-
-	html := fmt.Sprintf(metricsTemplate, hits)
-	_, err := w.Write([]byte(html))
-	if err != nil {
-		return
-	}
-}
-
-func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
-	cfg.fileServerHit.Store(0)
-
-	w.Header().Set("Content-Type", "text/plain")
-	_, err := w.Write([]byte("OK"))
-	if err != nil {
-		return
-	}
+	fileserverHits atomic.Int32
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	mux := http.NewServeMux()
-	apiCfg := &apiConfig{}
-	fileServer := http.FileServer(http.Dir(filepathRoot))
-	prefixStrip := http.StripPrefix("/app/", fileServer)
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
 
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(prefixStrip))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
 
-	// Metrics
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 
-	// Reset
-	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetrics)
-
-	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		_, err := w.Write([]byte("OK"))
-		if err != nil {
-			return
-		}
-		w.Header().Set("Content-Type", "text/plain")
-		w.Header().Set("charset", "utf-8")
-
-	})
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
